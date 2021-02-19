455cf53e07b80169ed2e99d3b2341d10d4f26033
Odrigor <rodcid.al@gmail.com>
2021-02-19 01:42:06 -0300

    init

--- cursoudemy/src/App.js
+++ cursoudemy/src/App.js
@@ -1,23 +1,31 @@
+import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
+
+
+function Hello(props) {
+    return <h2>{props.title}</h2>
+}
+
+class Text extends Component {
+  render() { 
+    return ( 
+      <div>
+        <p>{this.props.txt}</p>
+        <p>{this.props.Numb}</p>
+      </div>
+     );
+  }
+}
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <Hello title="Run motherFucker"/>
       </header>
+      <Text txt="esto esta mal xd" Numb='8'/>
     </div>
   );
 }
